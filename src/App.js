e769f646bb19f8dcb50d47555177e042b719451a
halimjounadi <106980783+halimjounadi@users.noreply.github.com>
2022-06-08 03:50:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import A from "./test.js";
+export default function App() {
+  return (
+    <div className="App">
+      <A />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
